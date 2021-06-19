33ab513dcac5602a113cfe5d4214f0f0f9d5b423
bedtreep <73709148+bedtreep@users.noreply.github.com>
2021-06-19 22:16:58 +0300

    Add files via upload

--- untitled.js
+++ untitled.js
@@ -9,7 +9,11 @@ var totalAmount = 0;
 var integer;
 var listItems = document.getElementsByTagName('li');
 var clear = document.getElementsByClassName("clear");
-
+var main = document.getElementsByClassName("main");
+var buttonColor = "rgb(10, 2, 107)";
+var nightIcon = document.getElementsByTagName("i");
+var darkMode;
+var header = document.getElementsByClassName("header");
 
 if(!localStorage.getItem("items")) {
 	console.log("bye");
@@ -23,6 +27,7 @@ function populateStorage() {
   localStorage.setItem("items", JSON.stringify(items));
   localStorage.setItem("totalsum", totalAmount);
   localStorage.setItem("listsOfItems", document.getElementsByTagName("ul")[0].innerHTML);
+  
   setStyles();
 }
 
@@ -30,6 +35,7 @@ function setStyles() {
 	var newitems = JSON.parse(localStorage.getItem("items"));
 	var newtotalAmount = localStorage.getItem("totalsum");
 	var newList = localStorage.getItem("listsOfItems");
+	var newDarkMode = localStorage.getItem("darkModeStatus");
 
 	while( list.firstChild ){
   list.removeChild( list.firstChild );
@@ -40,6 +46,34 @@ function setStyles() {
 		li.appendChild(document.createTextNode(newitems[k].join(" - ") + "₴"));
 		list.appendChild(li);
 	}*/
+	darkMode = newDarkMode;
+	if (darkMode == true || darkMode == "true") {
+		console.log("dark");
+		main[0].style.background = "#121212";
+		main[0].style.color = "white";
+		amount.style.color = "white";
+		amount.style.background = "#424242";
+		itemName.style.color = "white";
+		itemName.style.background = "#424242";
+		list.style.background = "#424242";
+		buttonColor = "#35afbd";
+		nightIcon[0].style.color = "white";	
+		header[0].style.background="linear-gradient(-45deg, #ffd15c, #61d63d, #0062f7)"
+		header[0].style.backgroundSize="400% 400%"
+	} else if (darkMode == false || darkMode == "false") {
+		console.log("light");
+		main[0].style.background = "white";
+		main[0].style.color = "black";
+		amount.style.color = "black";
+		amount.style.background = "#f1f0f0";
+		itemName.style.color = "black";
+		itemName.style.background = "#f1f0f0";
+		list.style.background = "#f1f0f0";
+		buttonColor = "rgb(10, 2, 107)";
+		nightIcon[0].style.color = "black";	
+		header[0].style.background="linear-gradient(-45deg, #291D98, #EC49E7, #FBBE62)"
+		header[0].style.backgroundSize="400% 400%"
+	}
 	items = newitems; 
 	list.innerHTML = localStorage.getItem("listsOfItems");
 	totalAmount = parseInt(newtotalAmount, 10);
@@ -101,14 +135,52 @@ function checkInputs () {
 		accept.style.cursor = "default";
 	} else {
 		accept.disabled = false;
-		accept.style.background = "#0a026b";
+		accept.style.background = buttonColor;
 		accept.style.cursor = "pointer";
 	}
 }
 
+function darkmodeEnable(){
+	if (darkMode == false || darkMode == "false") {
+		console.log("dark");
+		main[0].style.background = "#121212";
+		main[0].style.color = "white";
+		amount.style.color = "white";
+		amount.style.background = "#424242";
+		itemName.style.color = "white";
+		itemName.style.background = "#424242";
+		list.style.background = "#424242";
+		buttonColor = "#35afbd";
+		nightIcon[0].style.color = "white";	
+		header[0].style.background="linear-gradient(-45deg, #ffd15c, #61d63d, #0062f7)"
+		header[0].style.backgroundSize="400% 400%"
+		darkMode = true;
+		localStorage.setItem("darkModeStatus", darkMode);
+	} else if (darkMode == true || darkMode == "true") {
+		console.log("light");
+		main[0].style.background = "white";
+		main[0].style.color = "black";
+		amount.style.color = "black";
+		amount.style.background = "#f1f0f0";
+		itemName.style.color = "black";
+		itemName.style.background = "#f1f0f0";
+		list.style.background = "#f1f0f0";
+		buttonColor = "rgb(10, 2, 107)";
+		nightIcon[0].style.color = "black";	
+		header[0].style.background="linear-gradient(-45deg, #291D98, #EC49E7, #FBBE62)"
+		header[0].style.backgroundSize="400% 400%"
+		darkMode = false;
+		localStorage.setItem("darkModeStatus", darkMode);
+	}
+}
+
+
+
 checkInputs();
 amount.onchange = checkInputs;
 itemName.onchange = checkInputs;
+darkMode.onchange = populateStorage;
+nightIcon[0].addEventListener("click", darkmodeEnable);
 accept.addEventListener("click", newItem);
 accept.addEventListener("click", updateSum);
 accept.addEventListener("click", populateStorage);
